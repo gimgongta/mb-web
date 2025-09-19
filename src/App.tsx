a09c9bcf498ee07cbb2bc58dd7dfa80aefc80805
gimgongta <gimgongta@gmail.com>
2025-09-19 20:41:58 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,28 +3,28 @@ import { Link, Routes, Route } from 'react-router-dom'
 
 
 function Home() {
-return (
-<div>
-<h1>WebView App</h1>
-<p>React Native WebView로 띄울 정적 사이트</p>
-<nav>
-<Link to="/">Home</Link> | <Link to="/about">About</Link>
-</nav>
-</div>
-)
+  return (
+    <div>
+      <h1>WebView App</h1>
+      <p>React Native WebView로 띄울 정적 사이트</p>
+      <nav>
+        <Link to="/">Home</Link> | <Link to="/about">About</Link>
+      </nav>
+    </div>
+  )
 }
 
 
 function About() {
-return <div>About Page</div>
+  return <div>About Page</div>
 }
 
 
 export default function App() {
-return (
-<Routes>
-<Route path="/" element={<Home />} />
-<Route path="/about" element={<About />} />
-</Routes>
-)
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
+  )
 }
